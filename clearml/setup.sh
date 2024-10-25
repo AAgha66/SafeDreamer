36b9e66faf897131978f280081bd0b612f2379d0
AAgha66 <ahmed.agha@argmax.ai>
2024-10-25 10:34:34 +0200

    add setup.sh

---
+++ clearml/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Running setup script."
+
+# activate env
+source /root/miniconda3/bin/activate
+conda activate safedreamer-env
+
+# go to the repo directory
+cd $CLEARML_GIT_ROOT
+
+# now we need to tell clearml to use the python from our poetry env
+# this is in the general case (we use the system python above, so we could
+# have just hardcoded this as well)
+export python_path="/root/miniconda3/envs/safedreamer-env/bin/python"
+cat > $CLEARML_CUSTOM_BUILD_OUTPUT << EOL
+{
+  "binary": "$python_path",
+  "entry_point": "$CLEARML_GIT_ROOT/$CLEARML_TASK_SCRIPT_ENTRY",
+  "working_dir": "$CLEARML_GIT_ROOT/$CLEARML_TASK_WORKING_DIR"
+}
+EOL
